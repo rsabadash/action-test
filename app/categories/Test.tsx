f8d3a42d1fc272f178852d086eef0323ff31c135
rsabadash <r.v.sabadash@gmail.com>
2025-02-21 22:14:54 +0200

    Replace Next.js project with a Vite-based React setup

--- app/categories/Test.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
-
-export default function Test({ href, children }: any) {
-  console.log(usePathname());
-  return <Link href={href}>{children}</Link>;
-}
